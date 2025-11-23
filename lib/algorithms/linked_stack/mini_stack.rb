cf2e3ca89afae516248f06a9a4c36040c08ba3e6
huangmiao <huangmiao2468@gmail.com>
2025-11-23 15:22:43 +0800

    add note

--- lib/algorithms/linked_stack/mini_stack.rb
+++ lib/algorithms/linked_stack/mini_stack.rb
@@ -1,3 +1,28 @@
+=begin
+  一、最小栈（Min Stack）
+  1. 题目要干啥？
+  实现一个栈，支持四个操作：
+  - push(x)：压栈
+  - pop()：出栈
+  - top()：返回栈顶元素
+  - get_min()：返回栈中最小元素，要求 O(1) 时间复杂度
+  2. 核心思路：用两个栈
+  - 一个正常栈：存所有元素，叫 @stack
+  - 一个“最小栈”：存当前路径上的最小值，叫 @min_stack
+  规则：
+  - push(x)：
+      - 正常：@stack.push(x)
+      - 最小栈：
+          - 如果 @min_stack 为空，或者 x <= 当前最小值，就把 x 也压到 @min_stack
+  - pop：
+      - 正常栈弹出 val = @stack.pop
+      - 如果 val == @min_stack.last，说明当前最小值被弹掉了，@min_stack 也要 pop 一下
+  - top：@stack.last
+  - get_min：@min_stack.last
+  时间复杂度：所有操作都是 O(1)。
+=end
+
+
 module Algorithms
   module LinkedStack
     class MinStack
