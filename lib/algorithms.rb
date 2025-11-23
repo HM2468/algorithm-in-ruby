40740bb869ae58dc50b3721a1b865b4fa5878dca
huangmiao <huangmiao2468@gmail.com>
2025-11-23 08:42:51 +0800

    update

--- lib/algorithms.rb
+++ lib/algorithms.rb
@@ -1,13 +1,16 @@
 module Algorithms
   module Strings
-    # Require all string-related algorithms here
     require_relative "algorithms/strings/longest_palindrome"
     require_relative "algorithms/strings/longest_substring"
   end
 
   module LinkedLists
     require_relative "algorithms/linked-lists/basic"
-    require_relative "algorithms/linked-lists/operations"
+    require_relative "algorithms/linked-lists/detect_cycle"
+    require_relative "algorithms/linked-lists/has_cycle"
+    require_relative "algorithms/linked-lists/merge_two_sorted_list"
+    require_relative "algorithms/linked-lists/palindrome"
+    require_relative "algorithms/linked-lists/reverse_linked_list"
     require_relative "algorithms/linked-lists/reverse_k_group"
   end
 end
