c733ac317260f58e9e83a7170c3dea0d84715d57
huangmiao <huangmiao2468@gmail.com>
2026-02-07 00:04:27 +0800

    improve

--- lib/data_structure/linked_lists/06_merge_two_sorted_list.rb
+++ lib/data_structure/linked_lists/06_merge_two_sorted_list.rb
@@ -1,7 +1,15 @@
+=begin
+21. Merge Two Sorted Lists
+l1, l2 都是升序链表，返回升序合并后的头结点
+循环条件 l1_current && l2_current：两条都还有节点时才需要比较。每轮：
+  1.	比较 l1_current.val 和 l2_current.val
+  2.	把较小的那个接到 tail.next
+  3.	被接走的那条链表指针前进
+  4.	tail 前进到新接上的节点
+=end
+
 module DataStructure
   module LinkedLists
-    # 21. Merge Two Sorted Lists
-    # l1, l2 都是升序链表，返回升序合并后的头结点
     def merge_two_sorted_lists(l1, l2)
       dummy = Node.new(nil, nil)
       res_tail = dummy
